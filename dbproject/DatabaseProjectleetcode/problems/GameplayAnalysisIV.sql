07e2a8b88a09c94f4059d2f9e2a9e79af4969539
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-08-22 23:39:57 -0600

    game play analysis 4

--- dbproject/DatabaseProjectleetcode/problems/GameplayAnalysisIV.sql
+++ dbproject/DatabaseProjectleetcode/problems/GameplayAnalysisIV.sql
@@ -1,3 +1,8 @@
-select * from activity
+SELECT cast(COUNT(DISTINCT b.player_id) / (COUNT(DISTINCT a.player_id)  * 1.0) as DECIMAL(10,2)) as fraction FROM
+(
+select player_id, MIN(event_date) as event_date from activity
+GROUP BY player_id
+) AS A
+left JOIN Activity B ON a.player_id = b.player_id AND DATEADD(DAY, 1, a.event_date) = b.event_date
 
 
