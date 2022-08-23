f8cc83603ededa142f5a681799d151475d644bf3
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-08-23 01:05:51 -0600

    exchange seats

---
+++ dbproject/DatabaseProjectleetcode/dbo/Seat626.sql
@@ -0,0 +1,27 @@
+use leetcode
+GO
+Create table
+ Seat626
+(id int, student varchar
+(255))
+Truncate table Seat626
+insert into Seat626
+  (id, student)
+values
+  ('1', 'Abbot')
+insert into Seat626
+  (id, student)
+values
+  ('2', 'Doris')
+insert into Seat626
+  (id, student)
+values
+  ('3', 'Emerson')
+insert into Seat626
+  (id, student)
+values
+  ('4', 'Green')
+insert into Seat626
+  (id, student)
+values
+  ('5', 'Jeames')
